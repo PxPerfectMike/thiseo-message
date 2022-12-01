4b28e8333a558c8eb43584853360b9d062b0f807
michael baker <56899037+PxPerfectMike@users.noreply.github.com>
2022-11-30 19:03:12 -0800

    change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 			<header className='App-header'>
 				<img src={logo} className='App-logo' alt='logo' />
 				<p>
-					Deployed with CircleCi. Edit <code>src/App.js</code> and save to
+					Deployed with CircleCi b'y. Edit <code>src/App.js</code> and save to
 					reload.
 				</p>
 				<a
