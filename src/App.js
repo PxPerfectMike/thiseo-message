77ad2f62f3780848d2bb0ec217fb98ebcbeea1b2
michael baker <56899037+PxPerfectMike@users.noreply.github.com>
2022-11-30 17:10:41 -0800

    Deploying with circleci

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
 			<header className='App-header'>
 				<img src={logo} className='App-logo' alt='logo' />
 				<p>
-					Deployed with Heroku CLI-edit. Edit <code>src/App.js</code> and save
-					to reload.
+					Deployed with CircleCi. Edit <code>src/App.js</code> and save to
+					reload.
 				</p>
 				<a
 					className='App-link'
